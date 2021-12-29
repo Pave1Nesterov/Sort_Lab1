6da11abb01d272a2b740fcef74bffc7e6333d0f7
Pave1Nesterov <mrceed02@mail.ru>
2021-12-29 21:45:10 +0300

    Добавьте файлы проекта.

---
+++ Sort_Lab1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lab1_Sort
+{
+    class Program
+    {
+        public static long N_op = 0;
+        static void Main(string[] args)
+        {
+            // Стек на массиве с сортировкой распределяющим подсчётом (86 вариант)
+            var sw = new Stopwatch();
+            const int VALUES = 100, COUNT_OF_NUMBERS = 1000;
+            Stack stack = new Stack();
+            Random rnd = new Random();
+            for (int i = 0; i < COUNT_OF_NUMBERS; i++)
+                stack.Push(rnd.Next(1, VALUES));
+            stack.Print();
+
+            sw.Start();
+            Stack stack_2 = new Stack(); Program.N_op += 2;
+            Program.N_op += 2;
+            for (int i = 0; i < VALUES; i++)
+            {
+                Program.N_op += 2;
+                stack_2.Push(0);
+                Program.N_op += 2;
+            }
+            Program.N_op += 4;
+            for (int i = 0; i < stack.GetCount(); i++)
+            {
+                Program.N_op += 4;
+                stack_2.Set(stack_2.Get(stack.Get(i)) + 1, stack.Get(i));
+                Program.N_op++;
+            }
+
+            int pos = 0; Program.N_op++;
+            Program.N_op += 4;
+            for (int i = 0; i < stack_2.GetCount(); i++)
+            {
+                Program.N_op += 4;
+                Program.N_op += 5;
+                for (int j = 0; j < stack_2.Get(i); j++, pos++)
+                {
+                    Program.N_op += 5;
+                    stack.Set(i, pos);
+                    Program.N_op += 3;
+                }
+            }
+            sw.Stop();
+            stack.Print();
+            Console.WriteLine("N_op = " + N_op + " T_n = " + sw.Elapsed);
+        }
+    }
+}
---
+++ Sort_Lab1/Stack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1_Sort
+{
+    class Stack
+    {
+        private List<int> stack = new List<int>();
+        public int GetCount()
+        {
+            Program.N_op++;
+            return stack.Count;
+        }
+        public void Push(int value)
+        {
+            stack.Add(value); Program.N_op += 3;
+        }
+        public int Pop()
+        {
+            int value = stack[stack.Count - 1]; Program.N_op += 4;
+            stack.RemoveAt(stack.Count - 1); Program.N_op += 5;
+            return value;
+        }
+        public int Get(int position)
+        {
+            Stack tempStack = new Stack(); Program.N_op += 2;
+            Program.N_op += 4;
+            for (int i = GetCount() - 1; i > position; i--)
+            {
+                Program.N_op += 2;
+                tempStack.Push(Pop());
+                Program.N_op += 2;
+            }
+            int value = stack[position]; Program.N_op += 2;
+            Program.N_op += 4;
+            for (int i = tempStack.GetCount(); i > 0; i--)
+            {
+                Program.N_op += 2;
+                Push(tempStack.Pop());
+                Program.N_op += 2;
+            }
+            return value;
+        }
+        public void Set(int value, int position)
+        {
+            Stack tempStack = new Stack(); Program.N_op += 2;
+            Program.N_op += 4;
+            for (int i = GetCount() - 1; i > position; i--)
+            {
+                Program.N_op += 2;
+                tempStack.Push(Pop());
+                Program.N_op += 2;
+            }
+            stack[position] = value; Program.N_op += 2;
+            Program.N_op += 4;
+            for (int i = tempStack.GetCount(); i > 0; i--)
+            {
+                Program.N_op += 2;
+                Push(tempStack.Pop());
+                Program.N_op += 2;
+            }
+        }
+        public void Print()
+        {
+            foreach (int i in stack)
+                Console.Write(i + " ");
+            Console.WriteLine("____________________");
+        }
+    }
+}
